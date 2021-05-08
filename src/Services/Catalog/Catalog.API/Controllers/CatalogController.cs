54d36e0b079b91b915dd95992e9a99c6efc8a974
Mahtab Raza <mahtab.raza20020@gmail.com>
2021-05-08 13:14:25 -0700

    Basket Microservice developed

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,68 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            this._repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            var products = await _repository.GetProductByCategory(category);
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _repository.CreateProduct(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _repository.UpdateProduct(product));
+        }
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProductById(string id)
+        {
+            return Ok(await _repository.DeleteProduct(id));
+        }
+    }
+}
